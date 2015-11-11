9cab2bbe2997da718362e7d738679787837d80a4
YasminLago <ylagorebollar@danielcastelao.org>
2015-11-11 11:17:32 +0100

    Update ExemploWhile.java

--- ExemploWhile/src/exemplowhile/ExemploWhile.java
+++ ExemploWhile/src/exemplowhile/ExemploWhile.java
@@ -1,11 +1,16 @@
 package exemplowhile;
 //Yasmin
+import javax.swing.JOptionPane;
 public class ExemploWhile {
+    
     public static void main(String[] args) {
+        
         int nota1,nota2,nota3;
-        int i=0;
+        int i=0; 
+        int numAlumnos = Integer.parseInt(JOptionPane.showInputDialog(null,"Número de alumnos: "));
+       
        NotaMedia m=new NotaMedia();
-       while(i<30){
+       while(i<numAlumnos){
        nota1=m.getNota();
        nota2=m.getNota();
        nota3=m.getNota();
